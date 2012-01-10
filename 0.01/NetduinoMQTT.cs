eaba6fb0184bc98e48839e2aeff9922db0a69db0
Dan Anderson <dan-anderson@cox.net>
2012-01-10 12:49:44 -0600

    Added several new features, did a bunch of cleanup

--- 0.01/NetduinoMQTT.cs
+++ 0.01/NetduinoMQTT.cs
@@ -9,23 +9,47 @@ namespace Netduino_MQTT_Client_Library
 {
     static class Constants
     {
-    
+
         // TODO: Add other constants so we don't have "magic" numbers
         public const int MQTTPROTOCOLVERSION = 3;
         public const int MAXLENGTH = 268435455; // 256MB
+        public const int MAX_CLIENTID = 23;
+        public const int MIN_CLIENTID = 1;
+        public const int MAX_KEEPALIVE = 65535;
+        public const int MIN_KEEPALIVE = 0;
+        public const int MAX_USERNAME = 65535;
+        public const int MAX_PASSWORD = 65535;
+        public const int MAX_TOPIC_LENGTH = 32767;
+        public const int MIN_TOPIC_LENGTH = 1;
 
         // Error Codes
-        public const int CLIENTID_LENGTH_ERROR = -1;
-        public const int KEEPALIVE_LENGTH_ERROR = -2;
-        public const int MESSAGE_LENGTH_ERROR = -3;
-        public const int TOPIC_LENGTH_ERROR = -4;
-        public const int TOPIC_WILDCARD_ERROR = -5;
-        public const int USERNAME_LENGTH_ERROR = -6;
-        public const int PASSWORD_LENGTH_ERROR = -7;
+        public const int CLIENTID_LENGTH_ERROR = 1;
+        public const int KEEPALIVE_LENGTH_ERROR = 1;
+        public const int MESSAGE_LENGTH_ERROR = 1;
+        public const int TOPIC_LENGTH_ERROR = 1;
+        public const int TOPIC_WILDCARD_ERROR = 1;
+        public const int USERNAME_LENGTH_ERROR = 1;
+        public const int PASSWORD_LENGTH_ERROR = 1;
+        public const int CONNECTION_ERROR = 1;
+
+        public const int CONNECTION_OK = 0;
+
+        public const int CONNACK_LENGTH = 4;
+        public const int PINGRESP_LENGTH = 2;
+
+        public const byte MQTT_CONN_OK = 0x00;  // Connection Accepted
+        public const byte MQTT_CONN_BAD_PROTOCOL_VERSION = 0x01;  // Connection Refused: unacceptable protocol version
+        public const byte MQTT_CONN_BAD_IDENTIFIER = 0x02;  // Connection Refused: identifier rejected
+        public const byte MQTT_CONN_SERVER_UNAVAILABLE = 0x03;  // Connection Refused: server unavailable
+        public const byte MQTT_CONN_BAD_AUTH = 0x04;  //  Connection Refused: bad user name or password
+        public const byte MQTT_CONN_NOT_AUTH = 0x05;  //  Connection Refused: not authorized
 
         // Message types
         public const int MQTT_CONNECT_TYPE = 0x10;
+        public const int MQTT_CONNACK_TYPE = 0x20;
         public const int MQTT_PUBLISH_TYPE = 0x30;
+        public const int MQTT_PING_REQ_TYPE = 0xc0;
+        public const int MQTT_PING_RESP_TYPE = 0xd0;
         public const int MQTT_DISCONNECT_TYPE = 0xe0;
 
         // Flags
@@ -48,23 +72,30 @@ namespace Netduino_MQTT_Client_Library
             int fixedHeader = 0;
             int varHeader = 0;
             int payload = 0;
+            int returnCode = 0;
             bool usingUsername = false;
             bool usingPassword = false;
             byte connectFlags = 0x00;
             byte[] buffer = null;
 
+            UTF8Encoding encoder = new UTF8Encoding();
+
+            byte[] utf8ClientID = Encoding.UTF8.GetBytes(clientID);
+            byte[] utf8Username = Encoding.UTF8.GetBytes(username);
+            byte[] utf8Password = Encoding.UTF8.GetBytes(password);
+
             // Some Error Checking
             // ClientID improperly sized
-            if ((clientID.Length > 23) || (clientID.Length < 1))
+            if ((utf8ClientID.Length > Constants.MAX_CLIENTID) || (utf8ClientID.Length < Constants.MIN_CLIENTID))
                 return Constants.CLIENTID_LENGTH_ERROR;
             // KeepAlive out of bounds
-            if ((keepAlive > 65535) || (keepAlive < 0))
+            if ((keepAlive > Constants.MAX_KEEPALIVE) || (keepAlive < Constants.MIN_KEEPALIVE))
                 return Constants.KEEPALIVE_LENGTH_ERROR;
             // Username too long
-            if (username.Length > 65535)
+            if (utf8Username.Length > Constants.MAX_USERNAME)
                 return Constants.USERNAME_LENGTH_ERROR;
             // Password too long
-            if (password.Length > 65535)
+            if (utf8Password.Length > Constants.MAX_PASSWORD)
                 return Constants.PASSWORD_LENGTH_ERROR;
 
             // Check features being used
@@ -74,24 +105,24 @@ namespace Netduino_MQTT_Client_Library
                 usingPassword = true;
 
             // Calculate the size of the var header
-            varHeader += 2;                    // Protocol Name Length
-            varHeader += 6;                    // Protocol Name
-            varHeader++;                       // Protocol version
-            varHeader++;                       // Connect Flags
-            varHeader += 2;                    // Keep Alive 
+            varHeader += 2; // Protocol Name Length
+            varHeader += 6; // Protocol Name
+            varHeader++; // Protocol version
+            varHeader++; // Connect Flags
+            varHeader += 2; // Keep Alive
 
             // Calculate the size of the fixed header
-            fixedHeader++;                      // byte 1
-              
+            fixedHeader++; // byte 1
+
             // Calculate the payload
-            payload = clientID.Length + 2;
+            payload = utf8ClientID.Length + 2;
             if (usingUsername)
             {
-                payload += username.Length + 2;
+                payload += utf8Username.Length + 2;
             }
             if (usingPassword)
             {
-                payload += password.Length + 2;
+                payload += utf8Password.Length + 2;
             }
 
             // Calculate the remaining size
@@ -101,7 +132,7 @@ namespace Netduino_MQTT_Client_Library
             if (remainingLength > Constants.MAXLENGTH)
                 return Constants.MESSAGE_LENGTH_ERROR;
 
-            tmp = remainingLength; 
+            tmp = remainingLength;
 
             // Add space for each byte we need in the fixed header to store the length
             while (tmp > 0)
@@ -112,10 +143,10 @@ namespace Netduino_MQTT_Client_Library
             // End of Fixed Header
 
             // Build buffer for message
-            buffer = new byte[fixedHeader+varHeader+payload]; 
-            
+            buffer = new byte[fixedHeader + varHeader + payload];
+
             // Fixed Header (2.1)
-            buffer[index++] = Constants.MQTT_CONNECT_TYPE;    
+            buffer[index++] = Constants.MQTT_CONNECT_TYPE;
 
             // Encode the fixed header remaining length
             tmp = remainingLength;
@@ -125,27 +156,27 @@ namespace Netduino_MQTT_Client_Library
                 tmp = tmp / 128;
                 if (tmp > 0)
                 {
-                    digit = digit | Constants.CONTINUATION_BIT; 
+                    digit = digit | Constants.CONTINUATION_BIT;
                 }
                 buffer[index++] = (byte)digit;
             } while (tmp > 0);
             // End Fixed Header
-            
-            // Connect (3.1) 
+
+            // Connect (3.1)
             // Protocol Name
-            buffer[index++] = 0;                // String (MQIsdp) Length MSB - always 6 so, zeroed
-            buffer[index++] = 6;                // Length LSB
-            buffer[index++] = (byte)'M';        // M
-            buffer[index++] = (byte)'Q';        // Q
-            buffer[index++] = (byte)'I';        // I
-            buffer[index++] = (byte)'s';        // s
-            buffer[index++] = (byte)'d';        // d
-            buffer[index++] = (byte)'p';        // p
-            
+            buffer[index++] = 0; // String (MQIsdp) Length MSB - always 6 so, zeroed
+            buffer[index++] = 6; // Length LSB
+            buffer[index++] = (byte)'M'; // M
+            buffer[index++] = (byte)'Q'; // Q
+            buffer[index++] = (byte)'I'; // I
+            buffer[index++] = (byte)'s'; // s
+            buffer[index++] = (byte)'d'; // d
+            buffer[index++] = (byte)'p'; // p
+
             // Protocol Version
             buffer[index++] = Constants.MQTTPROTOCOLVERSION;
-            
-            // Connect Flags 
+
+            // Connect Flags
             // TODO: re-read the spec to figure out what the "will" stuff is about
             if (cleanSession)
                 connectFlags |= (byte)Constants.CLEAN_SESSION_FLAG;
@@ -156,43 +187,70 @@ namespace Netduino_MQTT_Client_Library
 
             // Set the connect flags
             buffer[index++] = connectFlags;
-            
+
             // Keep alive (defaulted to 20 seconds above)
-            buffer[index++] = (byte)(keepAlive / 255);   // Keep Alive MSB
-            buffer[index++] = (byte)(keepAlive % 255);   // Keep Alive LSB
-            
-            // ClientID 
-            buffer[index++] = (byte)(clientID.Length / 255);     // Length MSB
-            buffer[index++] = (byte)(clientID.Length % 255);     // Length LSB
-            for (var i = 0; i < clientID.Length; i++) 
+            buffer[index++] = (byte)(keepAlive / 255); // Keep Alive MSB
+            buffer[index++] = (byte)(keepAlive % 255); // Keep Alive LSB
+
+            // ClientID
+            buffer[index++] = (byte)(utf8ClientID.Length / 255); // Length MSB
+            buffer[index++] = (byte)(utf8ClientID.Length % 255); // Length LSB
+            for (var i = 0; i < utf8ClientID.Length; i++)
             {
-                buffer[index++] = (byte)clientID[i]; 
+                buffer[index++] = utf8ClientID[i];
             }
 
             // Username
             if (usingUsername)
             {
-                buffer[index++] = (byte)(username.Length / 255);     // Length MSB
-                buffer[index++] = (byte)(username.Length % 255);     // Length LSB
+                buffer[index++] = (byte)(utf8Username.Length / 255); // Length MSB
+                buffer[index++] = (byte)(utf8Username.Length % 255); // Length LSB
 
-                for (var i = 0; i < username.Length; i++)
+                for (var i = 0; i < utf8Username.Length; i++)
                 {
-                    buffer[index++] = (byte)username[i];
+                    buffer[index++] = utf8Username[i];
                 }
             }
 
             // Password
             if (usingPassword)
             {
-                buffer[index++] = (byte)(password.Length / 255);     // Length MSB
-                buffer[index++] = (byte)(password.Length % 255);     // Length LSB
+                buffer[index++] = (byte)(utf8Password.Length / 255); // Length MSB
+                buffer[index++] = (byte)(utf8Password.Length % 255); // Length LSB
 
-                for (var i = 0; i < password.Length; i++)
+                for (var i = 0; i < utf8Password.Length; i++)
                 {
-                    buffer[index++] = (byte)password[i];
+                    buffer[index++] = utf8Password[i];
                 }
             }
-            return mySocket.Send(buffer, index, 0); ;
+            
+            // Send the message
+            returnCode = mySocket.Send(buffer, index, 0);
+            
+            // The return code should equal our buffer length
+            if (returnCode != buffer.Length)
+            {
+                return Constants.CONNECTION_ERROR;
+            }
+
+            // Get the acknowledgement message
+            returnCode = mySocket.Receive(buffer, 0);
+            
+            // The length should equal 4
+            if (returnCode != Constants.CONNACK_LENGTH)
+            {
+                return Constants.CONNECTION_ERROR;
+            }
+            
+            // This should be a message type 2 (CONNACK)
+            if (buffer[0] == Constants.MQTT_CONNACK_TYPE)
+            {
+                // This is our return code from the server
+                return buffer[3];
+            }
+            
+            // If not zero = return the return code
+            return Constants.CONNECTION_ERROR;
         }
 
         // Publish a message to a broker (3.3)
@@ -208,31 +266,37 @@ namespace Netduino_MQTT_Client_Library
             int remainingLength = 0;
             byte[] buffer = null;
 
+            UTF8Encoding encoder = new UTF8Encoding();
+
+            byte[] utf8Topic = Encoding.UTF8.GetBytes(topic);
+
             // Some error checking
-            // Topic is too long or short
-            if ((topic.Length > 32767)||(topic.Length < 1))
-                return Constants.TOPIC_LENGTH_ERROR;
+            
             // Topic contains wildcards
-            if ((topic.IndexOf('#')!=-1)||(topic.IndexOf('+')!=-1))
+            if ((topic.IndexOf('#') != -1) || (topic.IndexOf('+') != -1))
                 return Constants.TOPIC_WILDCARD_ERROR;
+            
+            // Topic is too long or short
+            if ((utf8Topic.Length > Constants.MAX_TOPIC_LENGTH) || (utf8Topic.Length < Constants.MIN_TOPIC_LENGTH))
+                return Constants.TOPIC_LENGTH_ERROR;
 
             // Calculate the size of the var header
-            varHeader += 2;                    // Topic Name Length
-            varHeader += topic.Length;         // Topic Name
+            varHeader += 2; // Topic Name Length
+            varHeader += utf8Topic.Length; // Topic Name
 
             // Calculate the size of the fixed header
-            fixedHeader++;                      // byte 1
+            fixedHeader++; // byte 1
 
             // Calculate the payload
             payload = message.Length;
-            
+
             // Calculate the remaining size
             remainingLength = varHeader + payload;
 
             // Check that remaining length will fit into 4 encoded bytes
             if (remainingLength > Constants.MAXLENGTH)
                 return Constants.MESSAGE_LENGTH_ERROR;
-            
+
             // Add space for each byte we need in the fixed header to store the length
             tmp = remainingLength;
             while (tmp > 0)
@@ -243,14 +307,14 @@ namespace Netduino_MQTT_Client_Library
             // End of Fixed Header
 
             // Build buffer for message
-            buffer = new byte[fixedHeader + varHeader + payload]; 
-            
+            buffer = new byte[fixedHeader + varHeader + payload];
+
             // Start of Fixed header
-            //      Publish (3.3)
+            // Publish (3.3)
             buffer[index++] = Constants.MQTT_PUBLISH_TYPE;
-            
+
             // Encode the fixed header remaining length
-            tmp = remainingLength;  
+            tmp = remainingLength;
             do
             {
                 digit = tmp % 128;
@@ -261,16 +325,16 @@ namespace Netduino_MQTT_Client_Library
                 }
                 buffer[index++] = (byte)digit;
             } while (tmp > 0);
-            // End of fixed header 
+            // End of fixed header
 
-            // Start of Variable header 
+            // Start of Variable header
             // Length of topic name
-            buffer[index++] = (byte)(topic.Length / 255);  // Length MSB
-            buffer[index++] = (byte)(topic.Length % 255);  // Length LSB
+            buffer[index++] = (byte)(utf8Topic.Length / 255); // Length MSB
+            buffer[index++] = (byte)(utf8Topic.Length % 255); // Length LSB
             // Topic
-            for (var i = 0; i < topic.Length; i++)
+            for (var i = 0; i < utf8Topic.Length; i++)
             {
-                buffer[index++] = (byte)topic[i]; 
+                buffer[index++] = utf8Topic[i];
             }
             // End of variable header
 
@@ -294,6 +358,45 @@ namespace Netduino_MQTT_Client_Library
             buffer[1] = 0x00;
             return mySocket.Send(buffer, buffer.Length, 0);
         }
+
+        // Ping the MQTT broker - used to extend keep alive
+        public static int PingMQTT(Socket mySocket)
+        {
+
+            int index = 0;
+            int returnCode = 0;
+            byte[] buffer = null;
+
+            buffer = new byte[2];
+            buffer[index++] = Constants.MQTT_PING_REQ_TYPE; 
+            buffer[index++] = 0x00;
+
+            // Send the ping
+            returnCode = mySocket.Send(buffer, index, 0);
+
+            // The return code should equal our buffer length
+            if (returnCode != buffer.Length)
+            {
+                return Constants.CONNECTION_ERROR;
+            }
+
+            // Get the acknowledgement message
+            returnCode = mySocket.Receive(buffer, 0);
+
+            // The length should equal 2
+            if (returnCode != Constants.PINGRESP_LENGTH)
+            {
+                return Constants.CONNECTION_ERROR;
+            }
+
+            // This should be a message type 2 (CONNACK)
+            if (buffer[0] == Constants.MQTT_PING_RESP_TYPE)
+            {
+                return 0;
+            }
+            return Constants.CONNECTION_ERROR;
+        }
     }
 }
+
 
--- 0.01/Program.cs
+++ 0.01/Program.cs
@@ -12,31 +12,68 @@ namespace myNetduinoMQTT
 {
     public class Program
     {
+        
         public static void Main()
         {
-
-            Socket mySocket = ConnectSocket("192.168.1.106",1883);
-            NetduinoMQTT.ConnectMQTT(mySocket, "tester123", 10, true, "roger", "password");
-            NetduinoMQTT.PublishMQTT(mySocket, "test", "testme12");
-            Thread.Sleep(1000);
-            NetduinoMQTT.DisconnectMQTT(mySocket);
             
+            // setup our interrupt port (on-board button)
+            InterruptPort button = new InterruptPort(Pins.ONBOARD_SW1, false, Port.ResistorMode.Disabled, Port.InterruptMode.InterruptEdgeLow);
+
+            // assign our interrupt handler
+            button.OnInterrupt += new NativeEventHandler(button_OnInterrupt);
+
+            // go to sleep until the interrupt wakes us (saves power)
+            Thread.Sleep(Timeout.Infinite);
         }
-        
-        // From Sample Code (SockClient.cs) Copyright Microsoft
-        public static Socket ConnectSocket(String server, Int32 port)
+
+        // the interrupt handler 
+        static void button_OnInterrupt(uint data1, uint data2, DateTime time)
         {
-            // Get server's IP address.
-            IPHostEntry hostEntry = Dns.GetHostEntry(server);
+            int returnCode = 0;
+            int connectionError = 0;
+            // Get broker's IP address.
+            IPHostEntry hostEntry = Dns.GetHostEntry("192.168.1.106");
+            // Create socket and connect to the broker's IP address and port
+            Socket mySocket = new Socket(AddressFamily.InterNetwork,SocketType.Stream, ProtocolType.Tcp);
+            try
+            {
+                mySocket.Connect(new IPEndPoint(hostEntry.AddressList[0], 1883));
+            }
+            catch (SocketException SE)
+            {
+                Debug.Print("Connection Error");
+                connectionError = 1;
+            }
+            if (connectionError != 1)
+            {
+                // Send the connect message
+                returnCode = NetduinoMQTT.ConnectMQTT(mySocket, "tester\u00A5", 2, true, "roger\u00A5", "password\u00A5");
+                if (returnCode != 0)
+                {
+                    Debug.Print("Connection Error:");
+                    Debug.Print(returnCode.ToString());
+                }
+                else
+                {
+                    // Send our message
+                    NetduinoMQTT.PublishMQTT(mySocket, "test", "Ow! Quit it!");
 
-            // Create socket and connect to the server's IP address and port
-            Socket socket = new Socket(AddressFamily.InterNetwork,
-                SocketType.Stream, ProtocolType.Tcp);
-            socket.Connect(new IPEndPoint(hostEntry.AddressList[0], port));
-            return socket;
+                    for (int i = 0; i < 11; i++)
+                    {
+                        returnCode = NetduinoMQTT.PingMQTT(mySocket);
+                        if (returnCode == 0)
+                        {
+                            Debug.Print("Ping Received");
+                            Thread.Sleep(1000);
+                        }
+                    }
+                    Thread.Sleep(3000);
+                    // Send the disconnect message
+                    NetduinoMQTT.DisconnectMQTT(mySocket);
+                }
+                // Close the socket
+                mySocket.Close();
+            }
         }
-        // End of Microsoft Copyright sample code
-
-
     }
 }
